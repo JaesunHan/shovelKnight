ec9510b4f87bd43ae9a6575f48b4d120767f40d8
yjk3958 <ohsa2222>
2018-01-27 19:32:37 +0900

    중간반영

--- shovelKnight/playerBase+Move.cpp
+++ shovelKnight/playerBase+Move.cpp
@@ -6,39 +6,41 @@ void playerBase::move()
 
 	if (KEYMANAGER->isStayKeyDown('W'))
 	{
-		if (_state == PLAYERSTATE_HANG) _action = PLAYERACTION_MOVE;
-		_directionTB = PLAYERDIRECTION_TOP;
-	}
-	if (KEYMANAGER->isOnceKeyUp('W'))
-	{
-		if (_state == PLAYERSTATE_HANG)_action = PLAYERACTION_IDLE;
+		if (_state == PLAYERSTATE_HANG) _y -= 1;
 	}
 	if (KEYMANAGER->isStayKeyDown('S'))
 	{
-		if (_state == PLAYERSTATE_HANG) _action = PLAYERACTION_MOVE;
-		_directionTB = PLAYERDIRECTION_BOTTOM;
+		if (_state == PLAYERSTATE_HANG) _y += 1;
 	}
-	if (KEYMANAGER->isOnceKeyUp('S'))
-	{
-		if (_state == PLAYERSTATE_HANG)_action = PLAYERACTION_IDLE;
-	}
+
 	if (KEYMANAGER->isStayKeyDown('A'))
 	{
+		if (_action != PLAYERACTION_MOVE || _direction != PLAYERDIRECTION_LEFT) _currentFrameX = 0;
 		_direction = PLAYERDIRECTION_LEFT;
-		_action = PLAYERACTION_MOVE;
+		if (_state = PLAYERSTATE_ONLAND)_action = PLAYERACTION_MOVE;
+		if(_speed<MAXSPEED)_speed += ACCELERATION;
+		if (_speed > MAXSPEED)_speed = MAXSPEED;
 	}
 	if (KEYMANAGER->isOnceKeyUp('A'))
 	{
-		_action = PLAYERACTION_IDLE;
+		if (_action == PLAYERACTION_MOVE)_action = PLAYERACTION_IDLE;
+		if (_speed > 0)_speed -= ACCELERATION;
 	}
 	if (KEYMANAGER->isStayKeyDown('D'))
 	{
+		if (_state == PLAYERSTATE_ONLAND)
+		{
+			if (_action != PLAYERACTION_MOVE || _direction != PLAYERDIRECTION_RIGHT) _currentFrameX = 0;
+			if (_state = PLAYERSTATE_ONLAND)_action = PLAYERACTION_MOVE;
+		}
 		_direction = PLAYERDIRECTION_RIGHT;
-		_action = PLAYERACTION_MOVE;
+		if (_speed<MAXSPEED)_speed += ACCELERATION;
+		if (_speed > MAXSPEED)_speed = MAXSPEED;
 	}
 	if (KEYMANAGER->isOnceKeyUp('D'))
 	{
-		_action = PLAYERACTION_IDLE;
+		if(_action==PLAYERACTION_MOVE)_action = PLAYERACTION_IDLE;
+		if (_speed > 0)_speed -= ACCELERATION;
 	}
 
 	if (KEYMANAGER->isOnceKeyDown('J'))
@@ -47,31 +49,32 @@ void playerBase::move()
 	}
 	if (KEYMANAGER->isStayKeyDown('K'))
 	{
+		_isJump = true;
 		_action = PLAYERACTION_JUMP;
 		_jumpPower = JUMPPOWER;
 	}
+	if (_action == PLAYERACTION_JUMP&&_isJump)
+	{
+		_y -= _jumpPower;
+		_jumpPower -= _gravity;
+	}
 
-
+	_x += _moveSpeed * _direction;
 
 	switch (_state)
 	{
 	case PLAYERSTATE_ONLAND:
 	{
-		if (_action == PLAYERACTION_MOVE)_x += _moveSpeed * _direction;
 	}
 		break;
 	case PLAYERSTATE_INAIR:
 	{
-		_y -= _jumpPower;
-		_jumpPower -= _gravity;
-		if (_action == PLAYERACTION_MOVE)_x += _moveSpeed * _direction;
+		if(_action!=PLAYERACTION_ATTACK|| _action != PLAYERACTION_DAMAGED
+			|| _action != PLAYERACTION_DIE||_action!=PLAYERACTION_DOWNATTACK)_action = PLAYERACTION_JUMP;
 		if (_action == PLAYERACTION_JUMP && _jumpPower < 0) _currentFrameX = 1;
 		if (_action == PLAYERACTION_JUMP && _jumpPower > 0) _currentFrameX = 0;
 	}
 	break;
-	case PLAYERSTATE_HANG:
-		if (_action == PLAYERACTION_MOVE)_y += _moveSpeed * _directionTB;
-		break;
 	}
 
 	_rc = RectMake(_x - HIT_BOX_WIDTH / 2, _y - HIT_BOX_HEIGHT, HIT_BOX_WIDTH, HIT_BOX_HEIGHT);
--- shovelKnight/playerBase.h
+++ shovelKnight/playerBase.h
@@ -6,10 +6,11 @@
 #define ATTACK_DAMAGE_BOX_HEIGHT 6
 #define DOWNATTACK_DAMAGE_BOX_WIDTH 6
 #define	DOWNATTACK_DAMAGE_BOX_HEIGHT 10
-#define SPEED 4.0f
+#define ACCELERATION 0.4f
+#define MAXSPEED 2.0f
 #define TIMECOUNT 0.05f
-#define GRAVITY 0.f
-#define JUMPPOWER 0.f
+#define GRAVITY 0.01f
+#define JUMPPOWER 1.0f
 
 enum PLAYER_SKILL
 {
@@ -53,6 +54,7 @@ enum PLAYERACTION
 	PLAYERACTION_IDLE,
 	PLAYERACTION_MOVE,
 	PLAYERACTION_ATTACK,
+	PLAYERACTION_DOWNATTACK,
 	PLAYERACTION_JUMP,
 	PLAYERACTION_ROOTING,
 	PLAYERACTION_DAMAGED,
@@ -85,8 +87,8 @@ enum COLLISION_LR //충돌방향(좌우) ->대상기준
 
 class playerBase : public gameNode
 {
-protected : 
-	image*				_image;					//플레이어 이미지
+protected:
+	image * _image;					//플레이어 이미지
 	PLAYERDIRECTION		_direction;				//플레이어 상태
 	PLAYERDIRECTIONTB	_directionTB;			//
 	PLAYERSTATE			_state;					//플레이어 위치
@@ -110,6 +112,7 @@ protected :
 	int					_maxMP;					//최대 마나
 	int					_skill;					//스킬넘버
 	int					_alpha;					//알파값
+	float				_speed;
 	float				_time;
 	float				_x, _y;					//이동좌표	
 	float				_jumpPower;				//점프파워
@@ -118,6 +121,7 @@ protected :
 	bool				_rtBlock;				//오른쪽 막힘
 	bool				_ltBlock;				//왼쪽막힘
 	bool				_isDamaged;				//맞았는지
+	bool				_isJump;
 	bool				_skillUsed;				//스킬발동했는지
 	bool				_isDead;				//죽었냐?
 	int					probeY;
@@ -135,6 +139,7 @@ public:
 	void attack(float fireX, float fireY, bool skillUsed);
 	void move();
 	void imageSetting();
+	void collision();
 
 
 	inline float getX() { return _x; }
