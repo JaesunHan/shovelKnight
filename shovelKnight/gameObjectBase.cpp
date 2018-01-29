cc3331816f3089f21135caf2b72f5d5ec367ceeb
yjk3958 <ohsa2222>
2018-01-29 16:51:36 +0900

    오브젝트 생성 테스트 중간저장(필터)

--- shovelKnight/gameObjectBase.cpp
+++ shovelKnight/gameObjectBase.cpp
@@ -11,14 +11,9 @@ gameObjectBase::~gameObjectBase()
 {
 }
 
-HRESULT gameObjectBase::init(float x, float y, string imageKeyName, int dur)
+HRESULT gameObjectBase::init(float x, float y)
 {
-	_x = x;
-	_y = y;
-	_dur = dur;
-	_image = IMAGEMANAGER->findImage(imageKeyName);
-	_rc = RectMakeCenter(x, y, _image->getWidth(), _image->getHeight());
-	
+
 	return S_OK;
 }
 void gameObjectBase::release()
@@ -31,5 +26,38 @@ void gameObjectBase::update()
 }
 void gameObjectBase::render()
 {
-	//_image->render(getMemDC(), _rc.left, _rc.top);
+	
+}
+void gameObjectBase::settingObj(float x, float y, int dur, bool isDelete, COLORREF color)
+{
+	_x = x;
+	_y = y;
+	_dur = dur;
+	_isDelete = isDelete;
+	_color = color;
+	_rc = RectMakeCenter(x, y, _image->getWidth(), _image->getHeight());
+}
+void gameObjectBase::durAct()
+{
+
+
+}
+
+void gameObjectBase::collisionBox()
+{
+	HDC hdc = IMAGEMANAGER->findImage("bgMap")->getMemDC();
+	HBRUSH brush = CreateSolidBrush(_color);;
+	HBRUSH oBrush = (HBRUSH)SelectObject(hdc, brush);
+	HPEN pen = CreatePen(PS_SOLID, 1, _color);
+	HPEN oPen = (HPEN)SelectObject(hdc, pen);
+	Rectangle(hdc, CAMERAMANAGER->getX(_rc.left)
+		, CAMERAMANAGER->getY(_rc.top)
+		, CAMERAMANAGER->getX(_rc.right)
+		, CAMERAMANAGER->getY(_rc.bottom));
+	SelectObject(hdc, oBrush);
+	SelectObject(hdc, oPen);
+	DeleteObject(brush);
+	DeleteObject(pen);
+	DeleteObject(oBrush);
+	DeleteObject(oPen);
 }
